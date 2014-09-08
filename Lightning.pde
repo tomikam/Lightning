82abb4fbb14cba78b833c33c1d09721551044ed0
unknown <thwhite@697-334-Boromir.sf.k12.internal>
2014-09-08 11:36:15 -0700

    Changed a few things around for compadability

--- Lightning.pde
+++ Lightning.pde
@@ -5,10 +5,10 @@ int yStep;
 int i;
 int variation = 10;
 int tendancy = 0;
-//boolean drawBackground = false;
+
 int boltSize = 3;
 
-//Button small, medium, large, pretty;
+
 
 void setup()
 {
@@ -21,9 +21,7 @@ void draw()
 	int i = 0;
 	while(x < 600)
 		{
-			/*if (drawBackground) {
-				background(0);
-			}*/
+			
 			xStep = (int)(Math.random()* variation);
 			yStep = (int)(Math.random()* variation - (variation/2) + tendancy);
 			strokeWeight(boltSize);
@@ -34,11 +32,7 @@ void draw()
 			i++;
 		}
 
-	/*small = new Button(50, 500, 100, 50, 10);
-	medium = new Button();
-	large = new Button();
-	pretty = new Button();
-	small.show();*/
+	
 }
 void mousePressed()
 {
@@ -51,6 +45,7 @@ void mousePressed()
 	}
 	redraw();
 }
+
 void keyPressed()
 {
 	if (key == 'q' || key == 'Q') 
@@ -89,38 +84,10 @@ void keyPressed()
 			variation -=2;
 		}
 	}
-	/*else if (key == 'x' || key == 'X') {
-		drawBackground = !drawBackground;*/
-	}
+	
 }
 
-/*class Button
-{
-	int myX, myY, myL, myH, mytext;
-	Button(int x, int y, int l, int h, int btnText)
-	{
-		myX = x;
-		myY = y;
-		myL = l;
-		myH = h;
-		myText = btnText;
-	}
-	void checkIfInside() {
-		if (mouseX > myX && mouseX < myX + myL && mouseY > myY && mouseY < myY + myH) {
-			return true;
-		}
-		return false;
-	}
-	void show() 
-	{
-		fill(255, 125, 98);
-		rect(myX, myY, myL, myH);
-		stroke(0);
-		text(myText, myX + 10, myY + 10);
-	}
 
-}
-*/
 /** IDEAS */
 //Some element of control... maybe it goes where you click? 
 //It splits off at a random point?
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -21,10 +21,10 @@ int yStep;
 int i;
 int variation = 10;
 int tendancy = 0;
-boolean drawBackground = false;
+
 int boltSize = 3;
 
-//Button small, medium, large, pretty;
+
 
 public void setup()
 {
@@ -37,9 +37,7 @@ public void draw()
 	int i = 0;
 	while(x < 600)
 		{
-			if (drawBackground) {
-				background(0);
-			}
+			
 			xStep = (int)(Math.random()* variation);
 			yStep = (int)(Math.random()* variation - (variation/2) + tendancy);
 			strokeWeight(boltSize);
@@ -50,11 +48,7 @@ public void draw()
 			i++;
 		}
 
-	/*small = new Button(50, 500, 100, 50, 10);
-	medium = new Button();
-	large = new Button();
-	pretty = new Button();
-	small.show();*/
+	
 }
 public void mousePressed()
 {
@@ -67,6 +61,7 @@ public void mousePressed()
 	}
 	redraw();
 }
+
 public void keyPressed()
 {
 	if (key == 'q' || key == 'Q') 
@@ -105,38 +100,10 @@ public void keyPressed()
 			variation -=2;
 		}
 	}
-	else if (key == 'x' || key == 'X') {
-		drawBackground = !drawBackground;
-	}
+	
 }
 
-/*class Button
-{
-	int myX, myY, myL, myH, mytext;
-	Button(int x, int y, int l, int h, int btnText)
-	{
-		myX = x;
-		myY = y;
-		myL = l;
-		myH = h;
-		myText = btnText;
-	}
-	void checkIfInside() {
-		if (mouseX > myX && mouseX < myX + myL && mouseY > myY && mouseY < myY + myH) {
-			return true;
-		}
-		return false;
-	}
-	void show() 
-	{
-		fill(255, 125, 98);
-		rect(myX, myY, myL, myH);
-		stroke(0);
-		text(myText, myX + 10, myY + 10);
-	}
 
-}
-*/
 /** IDEAS */
 //Some element of control... maybe it goes where you click? 
 //It splits off at a random point?
